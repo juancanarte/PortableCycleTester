2775c9cd24de042ade20c6e950abcff3847ee2cf
juancanarte <juan.canarte@bi-kon.com>
2024-08-13 11:51:17 -0500

    Concatenar registros temporales 0.0.17

--- Proyecto_PCT/settings.py
+++ Proyecto_PCT/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-VERSION = '0.0.16'
+VERSION = '0.0.17'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
