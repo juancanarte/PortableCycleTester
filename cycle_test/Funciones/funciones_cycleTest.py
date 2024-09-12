c21dfc6379ea8b9ca137513c23cf24e29721cb7a
juancanarte <juan.canarte@bi-kon.com>
2024-09-12 10:04:41 -0500

    0.0.42

--- cycle_test/Funciones/funciones_cycleTest.py
+++ cycle_test/Funciones/funciones_cycleTest.py
@@ -305,13 +305,14 @@ def extraer_parametros(parametrosR, modo):
     global listaP, highValue_cafe_1, lowValue_cafe_1, highValue_cafe_2, lowValue_cafe_2
 
     parametros = parametrosR.split(',')
-    
+    print(parametros[37])
+    '''
     highValue_cafe_1 = int(parametros[35]) if parametros[35].isdigit() else 100
     lowValue_cafe_1 = int(parametros[36]) if parametros[36].isdigit() else 100
     highValue_cafe_2 = int(parametros[37]) if parametros[37].isdigit() else 100
     lowValue_cafe_2 = int(parametros[38]) if parametros[38].isdigit() else 100
 
-    '''
+    
     highValue_cafe_1 = int(parametros[35])
     lowValue_cafe_1 = int(parametros[36])
     highValue_cafe_2 = int(parametros[37])
