50c7dbecc136cf07b804b1efaedd98af76df0787
juancanarte <juan.canarte@bi-kon.com>
2024-09-13 09:40:47 -0500

    0.0.42

--- cycle_test/Funciones/funciones_cycleTest.py
+++ cycle_test/Funciones/funciones_cycleTest.py
@@ -997,8 +997,8 @@ def cycleTest_write_start():
                 porcent_step = scale_porcent
                 wtp_show = (widthTimePulse_show*1000)/(100/porcent_step)
                 wtp_show = widthTimePulse_show
-                array_1 = np.arange(0,101,porcent_step)
-                array_2 = np.arange(100, -1, -1*porcent_step)[1:-1]
+                array_1 = np.arange(setPoint_l_a,setPoint_h_a + 1,porcent_step)
+                array_2 = np.arange(setPoint_h_a, setPoint_l_a - 1, -1*porcent_step)[1:-1]
                 final_array = np.concatenate((array_1, array_2))
                 while not flag_a.is_set():
                     while pausa_hilo == True:
@@ -1017,8 +1017,8 @@ def cycleTest_write_start():
                 porcent_step = scale_porcent
                 wtp_show = (widthTimePulse_show*1000)/(100/porcent_step)
                 wtp_show = widthTimePulse_show
-                array_1 = np.arange(0,101,porcent_step)
-                array_2 = np.arange(100, -1, -1*porcent_step)[1:-1]
+                array_1 = np.arange(setPoint_l_a,setPoint_h_a + 1,porcent_step)
+                array_2 = np.arange(setPoint_h_a, setPoint_l_a - 1, -1*porcent_step)[1:-1]
                 final_array = np.concatenate((array_1, array_2))
                 while not flag_a.is_set():
                     while pausa_hilo == True:
