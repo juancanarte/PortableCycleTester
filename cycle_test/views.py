97d83be04d0ed6a5be36e7f5ead9d12b63cf1ad2
juancanarte <juan.canarte@bi-kon.com>
2024-09-24 16:57:52 -0500

    0.0.47

--- cycle_test/views.py
+++ cycle_test/views.py
@@ -289,13 +289,19 @@ def customTime_cafe_2(request):
 
 #Funcioes CYCLE TEST para COIL alone
 
-def sendData_coil_alone(request):
-    data = funciones_cycleTest.sendData_cafe_alone()
+@csrf_exempt
+def saveData_coil_alone(request):
+    if request.method == 'POST':
+        # Realiza tus validaciones y guarda los datos
+        # Si todo está bien
+        observations = request.POST.get('observations')
+        funciones_cycleTest.joinTemporalDB_a(observations)
 
-    print(data)
-    return JsonResponse({'dateStart_a': data['dateStart_a'], 'dateEnd_a': data['dateEnd_a'], 'counter_open_cafe_a':data['counter_open_cafe_a'],
-                         'counter_close_cafe_a':data['counter_close_cafe_a'], 'counter_openF_cafe_a':data['counter_openF_cafe_a'],
-                         'counter_closeF_cafe_a':data['counter_closeF_cafe_a'], 'customTime_a':data['customTime_a'], 'finalTime_a':data['finalTime_a']})
+        return JsonResponse({'success': True})
+        
+        # Si hubo un error
+        # return JsonResponse({'success': False, 'error': 'Mensaje de error'})
+    return JsonResponse({'success': False, 'error': 'Método no permitido'}, status=405)
 
 def stop(request):
     funciones_cycleTest.stop()
